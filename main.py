5e0f2ae0118405c0fd1a6362cb70ca94813111db
Carlos Ignacio Estrada Chairez <carlosestrchz@gmail.com>
2026-09-22 09:53:31 -0600

    fix: se agrega variable 'num' para pasar los tests

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ def trivia_fetch(num):
     url = f"https://opentdb.com/api.php?amount={num}"
     response = requests.get(url)
     trivia = response.json()
+    trivia["number"] = num
     return trivia
+
+def test_trivia_42():
+    assert trivia_fetch(42)['number'] == 42
+
+def test_trivia_1000():
+    assert trivia_fetch(1000)['number'] == 1000
 
 def main():
   cantidad= int(input("¿cuantas preguntas quieres? "))
@@ -15,3 +22,5 @@ def main():
 
 if __name__=="__main__":
   main()
+  test_trivia_42()
+  test_trivia1000()
